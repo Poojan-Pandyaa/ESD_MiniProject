2d943aa4d59fa7d57b3b5292330fea7833c49a1c
Poojan Pandya <poojan2861@gmail.com>
2026-01-27 10:12:46 +0530

    Refactor Helloworld.java to define main class

--- Helloworld.java
+++ Helloworld.java
@@ -1,3 +1,5 @@
-import java.utils.*;
-
-System.out.println("Hello World");
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
